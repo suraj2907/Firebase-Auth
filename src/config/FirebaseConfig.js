96453ead0acde3f2d3d4c00e5db44d094f469a09
Suraj Jawrani <surajjawrani2022@gmail.com>
2023-07-24 13:25:19 +0530

    deploying

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -1,4 +1,4 @@
-export default {
+const FirebaseConfig = {
   apiKey: "AIzaSyDW8RhaZj6xuIuURvxdi2Hn2Uqwlh998YU",
   authDomain: "myreactapp-1f072.firebaseapp.com",
   projectId: "myreactapp-1f072",
@@ -7,3 +7,5 @@ export default {
   appId: "1:482188289453:web:f351e4ad6be6293380ed24",
   measurementId: "G-H1CSCGCL3H",
 };
+
+export default FirebaseConfig;
